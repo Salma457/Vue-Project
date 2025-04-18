e627a828ce014a76150ecf52dbb02e95726450ba
abdelrahman-abobakr <abdelrahmanabobakr321@gmail.com>
2025-04-18 21:26:03 +0200

    add footer for adventure details

--- src/views/AdventureDetails.vue
+++ src/views/AdventureDetails.vue
@@ -12,6 +12,8 @@
         </div>    
         <ShortInfo :adventureSt="adventure" ></ShortInfo>
         <AdventureFullView class="ps-5" :fullInfo="adventure"></AdventureFullView>
+
+        <Footer></Footer>
     </div>
 </template>
 
@@ -21,7 +23,7 @@
     import Navbar from '@/components/Navbar.vue';  
     import ShortInfo from '@/components/AdventureDetails/ShortInfo.vue';
     import AdventureFullView from '@/components/AdventureDetails/AdventureFullView.vue';
-
+    import Footer from '@/components/Footer.vue';
     export default {
         data() {
             return {
@@ -33,7 +35,7 @@
         methods:{
 
         },
-        components:{Navbar, ShortInfo, AdventureFullView, },
+        components:{Navbar, ShortInfo, AdventureFullView,Footer },
 
         async created() {
             let _id = this.$route.params.id;
